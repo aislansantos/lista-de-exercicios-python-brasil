8546af99cf5d860ebd5252c5598e1c9a3aa74706
Aislan_Santos <aislan_85@yahoo.com.br>
2022-06-02 14:30:45 -0300

    'ex_03_imprima_soma_de_dois_numeros.py'

--- secao_01_estrutura_sequencial/ex_03_imprima_soma_de_dois_numeros.py
+++ secao_01_estrutura_sequencial/ex_03_imprima_soma_de_dois_numeros.py
@@ -14,4 +14,6 @@ Faça um Programa que peça dois números inteiros e imprima a soma.
 
 
 def imprima_a_soma_de_dois_numeros():
-    """Escreva aqui em baixo a sua solução"""
+    n1 = float(input('Digite um número: '))
+    n2 = float(input('Digite outro número: '))
+    print(f'A soma dos dois números informados é {n1+n2}')
