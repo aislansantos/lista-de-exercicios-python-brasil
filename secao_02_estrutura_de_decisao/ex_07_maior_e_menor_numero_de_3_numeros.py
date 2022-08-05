61fbc7dc2a3047439199344797c88ecdcc640f64
Aislan Oliveira <aislan.santos@gmail.com>
2022-08-05 08:54:28 -0300

    resolução ex_07_maior_e_menor_numero_de_3_numeros.py

--- secao_02_estrutura_de_decisao/ex_07_maior_e_menor_numero_de_3_numeros.py
+++ secao_02_estrutura_de_decisao/ex_07_maior_e_menor_numero_de_3_numeros.py
@@ -20,4 +20,22 @@ Faça um Programa que leia três números e mostre o maior e o menor deles.
 
 
 def calcular_maior_de_3_numeros(x, y, z):
-    """Escreva aqui em baixo a sua solução"""
+    n1 = x
+    n2 = y
+    n3 = z
+    if n1 < n2 and n1  < n3:
+        menor = n1
+    elif n2 < n1 and n2 < n3:
+        menor = n2
+    elif n3 < n1 and n3 < n1:
+        menor = n3
+
+    if n1 > n2 and n1 > n3:
+        maior = n1
+    elif n2 > n1 and n2 > n3:
+        maior = n2
+    elif n3 > n1 and n3 > n2:
+        maior = n3
+    
+    print(f'Maior: {maior}')
+    print(f'Menor: {menor}')
