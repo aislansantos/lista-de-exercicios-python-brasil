24234fc222d34c75e5d84efe9df6dda4f7f9eaa7
“Aislan” <aislan.santos@gmail.com>
2022-09-11 13:20:19 -0300

    resolução secao_02_estrutura_de_decisao/ex_18_validador_de_data.py

--- secao_02_estrutura_de_decisao/ex_18_validador_de_data.py
+++ secao_02_estrutura_de_decisao/ex_18_validador_de_data.py
@@ -20,7 +20,38 @@ Faça um Programa que peça uma data no formato dd/mm/aaaa e determine se a mesm
     'Data inválida'
 
 """
+from datetime import date, datetime
 
 
 def validar_data(data: str):
-    """Escreva aqui em baixo a sua solução"""
+    data = data
+    if len(data) > 10 or len(data) < 8:
+        return 'Data inválida'
+    data = data.split('/')
+    dia = int(data[0])
+    mes = int(data[1])
+    ano = int(data[2])
+    dias_31 = [1,3,5,7,8,10,12]
+    if mes < 1 or mes > 12:
+        return 'Data inválida'
+    if mes == 2:
+        if (ano % 4 == 0 and ano % 100 != 0) or ano % 400 == 0:
+            ultimo_dia = 29
+            if dia > ultimo_dia:
+                return 'Data inválida'
+        else:
+            ultimo_dia = 28
+            if dia > ultimo_dia:
+                return 'Data inválida'
+    else:
+        if mes in dias_31:
+            ultimo_dia = 31
+            if dia > ultimo_dia:
+                return 'Data inválida'
+        else:
+            ultimo_dia = 30
+            if dia > ultimo_dia:
+                return 'Data inválida'
+
+    return 'Data válida'
+
