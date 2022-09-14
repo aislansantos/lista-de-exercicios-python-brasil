c0753dfecd8716cdbc5ae2775d6e6a3e69bee35b
Aislan <aislan_85@yahoo.com.br>
2022-09-14 10:54:08 -0300

    resolução ex_22_par_ou_impar.py

--- secao_02_estrutura_de_decisao/ex_22_par_ou_impar.py
+++ secao_02_estrutura_de_decisao/ex_22_par_ou_impar.py
@@ -21,4 +21,8 @@ Faça um Programa que peça um número inteiro e determine se ele é par ou impa
 
 
 def decidir_se_eh_par_ou_impar(valor: int) -> str:
-    """Escreva aqui em baixo a sua solução"""
+    valor = valor
+    if valor % 2 == 0:
+        return "Par"
+    else:
+        return "Impar"
